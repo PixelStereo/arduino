fd0616f112e4390241d39c370be3b2cc4b82acf0
reno- <contact@rubiano.fr>
2015-02-20 16:35:58 +0100

    2 nodes artnet pour mobiles

--- mobile_ArtnetNeoPixel_2/mobile_ArtnetNeoPixel_2.ino
+++ mobile_ArtnetNeoPixel_2/mobile_ArtnetNeoPixel_2.ino
@@ -1,5 +1,5 @@
 /*
-This example will receive multiple universes via Artnet and control a strip of ws2811 leds via 
+This example will receive multiple universes via Artnet and control a strip of ws2811 leds via
 Adafruit's NeoPixel library: https://github.com/adafruit/Adafruit_NeoPixel
 This example may be copied under the terms of the MIT license, see the LICENSE file for details
 */
@@ -18,7 +18,7 @@ Adafruit_NeoPixel leds = Adafruit_NeoPixel(numLeds, dataPin, NEO_GRB + NEO_KHZ80
 
 // Artnet settings
 Artnet artnet;
-const int startUniverse = 1; // CHANGE FOR YOUR SETUP most software this is 1, some software send out artnet first universe as 0.
+const int startUniverse = 0; // CHANGE FOR YOUR SETUP most software this is 1, some software send out artnet first universe as 0.
 
 // Check if we got all universes
 const int maxUniverses = numberOfChannels / 512 + ((numberOfChannels % 512) ? 1 : 0);
@@ -27,12 +27,11 @@ bool sendFrame = 1;
 int previousDataLength = 0;
 
 // Change ip and mac address for your setup
-byte ip[] = {10, 0, 0, 11};
-byte mac[] = {0x90, 0xA2, 0xDA, 0x0E, 0xF3, 0x62};
+byte ip[] = {10, 0, 0, 12};
+byte mac[] = {0x90, 0xA2, 0xDA, 0x0D, 0xAC, 0xA8};
 
 void setup()
 {
-  //Serial.begin(115200);
   artnet.begin(mac, ip);
   leds.begin();
   initTest();
@@ -49,42 +48,13 @@ void loop()
 
 void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* data)
 {
-  sendFrame = 1;
-  // set brightness of the whole strip 
-  if (universe == 15)
-  {
-    leds.setBrightness(data[0]);
-    leds.show();
-  }
-
-  // Store which universe has got in
-  if (universe < maxUniverses)
-    universesReceived[universe] = 1;
-
-  for (int i = 0 ; i < maxUniverses ; i++)
-  {
-    if (universesReceived[i] == 0)
+  if (universe == 1)  {
+    // read universe and put into the right part of the display buffer
+    for (int i = 0; i < numLeds; i++)
     {
-      //Serial.println("Broke");
-      sendFrame = 0;
-      break;
+      leds.setPixelColor(i, data[i * 3], data[i * 3 + 1], data[i * 3 + 2]);
     }
-  }
-
-  // read universe and put into the right part of the display buffer
-  for (int i = 0; i < length / 3; i++)
-  {
-    int led = i + (universe - startUniverse) * (previousDataLength / 3);
-    if (led < numLeds)
-      leds.setPixelColor(led, data[i * 3], data[i * 3 + 1], data[i * 3 + 2]);
-  }
-  previousDataLength = length;     
-  
-  if (sendFrame)
-  {
     leds.show();
-    // Reset universeReceived to 0
-    memset(universesReceived, 0, maxUniverses);
   }
 }
 
